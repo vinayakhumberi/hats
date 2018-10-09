504bb835a7cecb0d68ca24d77888784ae6d5e73c
vinayakhumberi <vinay.humberi@gmail.com>
2018-10-09 18:29:41 +0530

    adding UI for the logic

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import io from 'socket.io-client';
+var _ = require('lodash');
 
 const socket = io('http://localhost:9000');
 
+let result = [];
 let socketID = '';
-
 const isPrime = num => {
   for(let i = 2, s = Math.sqrt(num); i <= s; i++)
       if(num % i === 0) return false; 
   return num !== 1;
 }
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      result: [],
+      start: 0,
+      end: 0,
+    };
+    this.computePrime = this.computePrime.bind(this);
+  }
   componentDidMount() {
     socket.on('connect', () => {
       socketID = socket.id;
@@ -21,6 +30,7 @@ class App extends Component {
     socket.on('findPrime', ((data) => {
       const range = data.table[socketID];
       const result = [];
+      console.log('Requested', data);
       for(let i = range.range.start; i <= range.range.end ; i++){
         if (isPrime(i)) {
           result.push(i);
@@ -33,17 +43,26 @@ class App extends Component {
       });
     }));
     socket.on('result', ((data) => {
-      console.log(data);
+      data.map((v) => {
+        result.push(v);
+      });
+      result = result.sort((a, b) =>   {
+        return a - b;
+      });
+      this.setState({
+        result: _.sortedUniq(result),
+      });
     }));
   }
   computePrime() {
     const data = {
       id: socketID,
       data: {
-        start: 0,
-        end: 100,
+        start: this.state.start,
+        end: this.state.end,
       }
     }
+    result = [];
     socket.emit('computePrime', data);
   }
   render() {
@@ -52,10 +71,43 @@ class App extends Component {
         <header className="App-header">
           <h1 className="App-title">Find Prime Numbers !</h1>
         </header>
+        <div className={'formInput'}>
+          <label forhtml="startPrime">Start number</label>
+          <input
+            type="number"
+            name="startPrime"
+            value={this.state.start}
+            onChange={(e) => {
+              const start = e.target.value;
+              this.setState({
+                start,
+              });
+            }}
+          />
+        </div>
+        <div className={'formInput'}>
+          <label forhtml="endPrime">End number</label>
+          <input
+            type="number"
+            name="endPrime"
+            value={this.state.end}
+            onChange={(e) => {
+              const end = e.target.value;
+              this.setState({
+                end,
+              });
+            }}
+          />
+        </div>
         <div className="body">
-          <button onClick={this.computePrime} >
-            ComputePrime
+          <button onClick={this.computePrime}>
+            Compute Prime
           </button>
+          <div className="result">
+            <p>
+              {this.state.result.join(', ')}
+            </p>
+          </div>
         </div>
       </div>
     );
